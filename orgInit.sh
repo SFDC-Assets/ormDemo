536d9ca37fac7d7c5b2256ebea1de5de96d138df
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2021-10-03 10:08:26 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --userprefix operational -o risk.mgmt
 sfdx force:source:push
-sfdx automig:load ./data
+sfdx automig:load -d ./data
 sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:user:permset:assign -n orm_app
 sfdx force:org:open
