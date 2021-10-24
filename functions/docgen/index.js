bf935989297fb9a479fdae2f1343ab9cce026fa6
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-10-24 14:41:25 -0700

    update

--- functions/docgen/index.js
+++ functions/docgen/index.js
@@ -1,3 +1,4 @@
+// Utilize JS library for document generation
 import { jsPDF } from "jspdf";
 
 /**
--- orgInit.sh
+++ orgInit.sh
@@ -3,7 +3,21 @@ sfdx force:source:push
 sfdx automig:load -d ./data
 sfdx shane:user:password:set -p salesforce1 -g User -l User
 sfdx force:user:permset:assign -n orm_app
+sfdx force:user:permset:assign -n Functions
+sfdx force:user:create -f users/user1.xml
+sfdx force:user:create -f users/user2.xml
+sfdx force:user:create -f users/user3.xml
 sfdx force:org:open
+
+## Login to functions
+# sf login functions
+## Create functions environment
+# sf env create compute -o orm -a docGenCompute
+## commit functions first
+#sf deploy functions -o orm
+## 
+# 
 
 # Test command
 # echo "GenerateRiskDoc.test('Alice');" | sfdx force:apex:execute -f /dev/stdin
+
