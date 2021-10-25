fe45ae7b2bff10cd341134494a826ece453940ee
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-10-25 12:39:31 -0700

    update

--- functions/docgen/index.js
+++ functions/docgen/index.js
@@ -1,5 +1,5 @@
-import labelmake from "labelmake";
-import jsPDF from "jspdf";
+// import labelmake from "labelmake";
+// import jsPDF from "jspdf";
 
 /**
  * Generates a risk document from the questions on a risk review record
@@ -19,37 +19,8 @@ export default async function (event, context, logger) {
     const { recordId, body } = event.data;
     logger.info(body);
 
-    // const { jsPDF } = require("jspdf");
+    const { jsPDF } = require("jspdf");
     // const jsPDF = require('jspdf/dist/jspdf.node.debug')
-
-    const template = {
-        basePdf: { width: 210, height: 297 },
-        schemas: [
-          {
-            field1: {
-              position: { x: 20, y: 20 },
-              width: 50,
-              height: 50,
-              fontSize: 30,
-              type: "text"
-            },
-            field2: {
-              position: { x: 20, y: 35 },
-              width: 50,
-              height: 50,
-              fontSize: 20,
-              type: "text"
-            }
-          }
-        ]
-      };
-      const inputs = [
-        { field1: "aa", field2: "aaaaaaaaaaaa" },
-        { field1: "bb", field2: "bbbbbbbbbbbb" }
-      ];
-      const pdf = await labelmake({ template, inputs });
-      const blob = new Blob([pdf.buffer], { type: "application/pdf" });
-      document.getElementById("iframe").src = URL.createObjectURL(blob);    
 
     // const doc = new jsPDF();
     // doc.text("Hello world!", 10, 10);
