486152b807aeba15b7a03ce54a749f8a4dd538a0
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-10-25 14:09:46 -0700

    update

--- functions/docgen/index.js
+++ functions/docgen/index.js
@@ -19,8 +19,8 @@ const { jsPDF } = require("jspdf");
  
 export default async function (event, context, logger) {
     logger.info(`Invoking Docgen with payload ${JSON.stringify(event.data || {})}`);
-    // const { recordId, body } = event.data;
-    // logger.info(body);
+    const { recordId, body } = event.data;
+    logger.info(body);
 
     // const { jsPDF } = require("jspdf");
     // const jsPDF = require('jspdf/dist/jspdf.node.debug')
