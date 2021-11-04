21059215b364282bd4761f519939a18cdb97e3d5
Jonathan Zhou <jonathan.zhou@jzhou-ltm1.internal.salesforce.com>
2021-11-03 21:39:35 -0700

    update

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/functions-scratch-def.json -a orm
+sfdx force:org:create -f config/functions-scratch-def.json -a orm -d 30
 sfdx force:source:push
 sfdx automig:load -d ./data
 sfdx shane:user:password:set -p salesforce1 -g User -l User
